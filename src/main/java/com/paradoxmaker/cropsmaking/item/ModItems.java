5e27304f41563b2c4d250f146b8a1ef4834745d2
BIMA <paradoxmaker083@gmail.com>
2022-09-06 13:29:51 +0700

    Creating MORE ITEMS

--- src/main/java/com/paradoxmaker/cropsmaking/item/ModItems.java
+++ src/main/java/com/paradoxmaker/cropsmaking/item/ModItems.java
@@ -13,7 +13,31 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, Cropsmaking.MODID);
 
     public static final RegistryObject<Item> BANANA = ITEMS.register("banana",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> SHALLOT = ITEMS.register("shallot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> GARLIC = ITEMS.register("garlic",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> CASAVA = ITEMS.register("casava",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> CUCUMBER = ITEMS.register("cucumber",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> CHILLI = ITEMS.register("chilli",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> LETTUCE = ITEMS.register("lettuce",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> CABBAGE = ITEMS.register("cabbage",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> tomato = ITEMS.register("tomato",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
 
     public static final RegistryObject<Item> banasword = ITEMS.register("banasword",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).durability(100)));
