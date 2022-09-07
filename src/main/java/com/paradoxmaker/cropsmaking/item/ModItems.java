d2773437323781d2dd5775ba4b1efe7a1b32ebff
BIMA <paradoxmaker083@gmail.com>
2022-09-07 22:58:30 +0700

    Added fishes, fishing loot Table added Tuna, add Tuna Crafting recipe and cooked sliced_tuna

--- src/main/java/com/paradoxmaker/cropsmaking/item/ModItems.java
+++ src/main/java/com/paradoxmaker/cropsmaking/item/ModItems.java
@@ -1,6 +1,10 @@
 package com.paradoxmaker.cropsmaking.item;
 
 import com.paradoxmaker.cropsmaking.Cropsmaking;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -41,6 +45,35 @@ public class ModItems {
 
     public static final RegistryObject<Item> banasword = ITEMS.register("banasword",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).durability(100)));
+
+
+    public static final RegistryObject<Item> tuna = ITEMS.register("tuna",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> RAW_TUNA = ITEMS.register("raw_tunas",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder()
+                    .nutrition(2).saturationMod(4f).build())));
+
+    public static final RegistryObject<Item> cooked_tuna = ITEMS.register("cooked_tuna",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder()
+                    .nutrition(8).saturationMod(6f)
+                    .effect(()->new MobEffectInstance(MobEffects.DOLPHINS_GRACE),10f).build())));
+
+    public static final RegistryObject<Item> raw_squid = ITEMS.register("raw_squid",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder()
+                    .nutrition(1).saturationMod(2f).build())));
+
+    public static final RegistryObject<Item> cooked_squid = ITEMS.register("cooked_squid",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder()
+                    .nutrition(6).saturationMod(4f).build())));
+
+    public static final RegistryObject<Item> shrimp = ITEMS.register("shrimp",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder()
+                    .nutrition(1).saturationMod(4f).build())));
+
+    public static final RegistryObject<Item> cooked_shrimp = ITEMS.register("cooked_shrimp",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder()
+                    .nutrition(6).saturationMod(4f).build())));
     public static void register(IEventBus eb){
         ITEMS.register(eb);
     }
