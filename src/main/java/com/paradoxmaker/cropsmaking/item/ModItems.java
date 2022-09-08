23cfd172660b2aa02f48ecb1a66c72ff20db3cd8
BIMA <paradoxmaker083@gmail.com>
2022-09-08 14:25:23 +0700

    Added All Items (So Far), fixing Fishing loot

--- src/main/java/com/paradoxmaker/cropsmaking/item/ModItems.java
+++ src/main/java/com/paradoxmaker/cropsmaking/item/ModItems.java
@@ -16,9 +16,178 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Cropsmaking.MODID);
 
+    //Fruit
     public static final RegistryObject<Item> BANANA = ITEMS.register("banana",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
 
+    public static final RegistryObject<Item> AVOCADO = ITEMS.register("avocado",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> DRAGONF = ITEMS.register("dragonf",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> DURIAN = ITEMS.register("durian",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> GUAVA = ITEMS.register("guava",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> LEMON = ITEMS.register("lemon",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> MANGO = ITEMS.register("mango",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> ORANGE = ITEMS.register("orange",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    //Juice
+    public static final RegistryObject<Item> APPLE_JUICE = ITEMS.register("apple_juice",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> AVOCADO_JUICE = ITEMS.register("avocado_juice",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> BANANA_JUICE = ITEMS.register("banana_juice",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> DRAGON_JUICE = ITEMS.register("dragon_juice",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> GUAVA_JUICE = ITEMS.register("guava_juice",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> LEMON_JUICE = ITEMS.register("lemon_juice",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> MANGO_JUICE = ITEMS.register("mango_juice",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> ORANGE_JUICE = ITEMS.register("orange_juice",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+    //Food
+    public static final RegistryObject<Item> BREAKFAST_TACO = ITEMS.register("breakfast_taco",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> BRENGKES = ITEMS.register("brengkes",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> BRENGKES_TEMPOYAK = ITEMS.register("brengkes_tempoyak",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> BURGER = ITEMS.register("burger",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> CARNITAS_TACO = ITEMS.register("carnitas_taco",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> CASSAVA_CHIP = ITEMS.register("cassava_chip",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> CHEESE_SAND = ITEMS.register("cheese_sand",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> CHICKEN_SAND = ITEMS.register("chicken_sand",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> DONUT = ITEMS.register("donut",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> DOUGH = ITEMS.register("dough",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> DX_OMELETTE = ITEMS.register("dx_omelette",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> enchilada = ITEMS.register("enchilada",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> FRIED_CHICKEN = ITEMS.register("fried_chicken",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> FRIED_EGG = ITEMS.register("fried_egg",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> FRIES = ITEMS.register("fries",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> GARLIC_BREAD = ITEMS.register("garlic_bread",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> GUACAMOLE = ITEMS.register("guacamole",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> GUACVEG_TACO = ITEMS.register("guacveg_taco",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> MASH_POTATO = ITEMS.register("mash_potato",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> OKONOMIYAKI = ITEMS.register("okonomiyaki",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> OMELETTE = ITEMS.register("omelette",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> PANCAKE = ITEMS.register("pancake",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> PIZZA = ITEMS.register("pizza",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> POTATO_CHIP = ITEMS.register("potato_chip",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> REUBEN_SAND = ITEMS.register("reuben_sand",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> SASHIMI = ITEMS.register("sashimi",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> SPICY_OMELETTE = ITEMS.register("spicy_omelette",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> SPICY_WING = ITEMS.register("spicy_wing",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> TAKOYAKI = ITEMS.register("takoyaki",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> TEMPURA = ITEMS.register("tempura",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> TORTILLA = ITEMS.register("tortilla",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> TUNA_SAND = ITEMS.register("tuna_sand",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    //Processed
+    public static final RegistryObject<Item> CHEESE = ITEMS.register("cheese",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> FLOUR = ITEMS.register("flour",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> MAYO = ITEMS.register("mayo",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> SALT = ITEMS.register("salt",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> SAMBAL = ITEMS.register("sambal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> SAUERKRAUT = ITEMS.register("sauerkraut",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> TAPIOCA = ITEMS.register("tapioca",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static final RegistryObject<Item> TOMATO_SAUCE = ITEMS.register("tomato_sauce",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    //Crops
     public static final RegistryObject<Item> SHALLOT = ITEMS.register("shallot",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
 
@@ -43,10 +212,7 @@ public class ModItems {
     public static final RegistryObject<Item> tomato = ITEMS.register("tomato",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
 
-    public static final RegistryObject<Item> banasword = ITEMS.register("banasword",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).durability(100)));
-
-
+    //Fishes
     public static final RegistryObject<Item> tuna = ITEMS.register("tuna",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
 
@@ -74,6 +240,11 @@ public class ModItems {
     public static final RegistryObject<Item> cooked_shrimp = ITEMS.register("cooked_shrimp",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder()
                     .nutrition(6).saturationMod(4f).build())));
+
+    //Other
+
+    public static final RegistryObject<Item> banasword = ITEMS.register("banasword",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).durability(100)));
     public static void register(IEventBus eb){
         ITEMS.register(eb);
     }
